d6fca564b0d326bfa438254b7e97f43db4977cf4
imorton <imorton93@live.ca>
2025-03-09 18:03:58 -0700

    fixed bug: user refreshes on recipe child routes and the modal disappears

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,25 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
-import { filter } from 'rxjs/operators';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
   styleUrl: './recipes.component.css'
 })
-export class RecipesComponent {
+export class RecipesComponent implements OnInit {
   isModalOpen = false;
 
   constructor(private router: Router, private route: ActivatedRoute) {  }
 
   ngOnInit() {
+    // Handle case when component initializes (refresh/manual URL input)
+    this.checkActiveChildRoute();
+
+    // Handle route changes dynamically
     this.router.events
-      .pipe(filter((event) => event instanceof NavigationEnd))
-      .subscribe(() => {
-        // Open modal if any child route is active (i.e., new, :id, :id/edit)
-        const childRouteActive = this.route.firstChild?.snapshot.url.length;
-        this.isModalOpen = !!childRouteActive;
-      });
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => this.checkActiveChildRoute());
+  }
+
+  checkActiveChildRoute() {
+    this.isModalOpen = this.route.children.some(child => child.snapshot.url.length > 0);
   }
 
 
